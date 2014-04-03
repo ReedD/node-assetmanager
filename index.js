ef55a175c2e7fa25399a79eee80087c7b1bbf059
Reed Dadoune <r@thriveline.com>
2014-04-02 17:06:41 -0700

    Added further consolidation and version update for npm

--- index.js
+++ index.js
@@ -9,94 +9,83 @@
 'use strict';
 
 var glob = require('glob'),
-  fs = require('fs'),
-  _ = require('underscore');
+	fs = require('fs'),
+	_ = require('underscore');
 
 // Asset holder variable
 var assets = {
-  css: [],
-  js: []
+	css: [],
+	js: []
 };
 
 exports.init = function (options) {
-  // Glob options
-  var globOptions = {sync: true};
+	// Glob options
+	var globOptions = {sync: true};
 
-  options = _.extend({
-    css: {},
-    js: {},
-    debug: true,
-    webroot: false
-  }, options);
+	options = _.extend({
+		css: {},
+		js: {},
+		debug: true,
+		webroot: false
+	}, options);
 
-  /**
-   * Filter out assets that are not files
-   *
-   * @param files
-   */
-  var filterFiles = function (files) {
-    return _.filter(files, function (file) {
-      return fs.statSync(file).isFile();
-    });
-  };
+	/**
+	 * Filter out assets that are not files
+	 *
+	 * @param files
+	 */
+	var filterFiles = function (files) {
+		return _.filter(files, function (file) {
+			return fs.statSync(file).isFile();
+		});
+	};
 
-  /**
-   * Get assets from pattern. Pattern could be
-   *  - an array
-   *  - a string
-   *  - external resource
-   *
-   * @param pattern
-   */
-  var getAssets = function (pattern) {
-    var files = [];
-    if (_.isArray(pattern)) {
-      _.each(pattern, function (path) {
-        files = files.concat(getAssets(path));
-      });
-    } else if (_.isString(pattern)) {
-      var regex = new RegExp('^(http://|https://|//)');
-      if (regex.test(pattern)) {
-        // Source is external
-        files.push(pattern);
-      } else {
-        glob(pattern, globOptions, function (er, matches) {
-          files = filterFiles(matches);
-        });
-      }
-    }
+	/**
+	 * Get assets from pattern. Pattern could be
+	 *  - an array
+	 *  - a string
+	 *  - external resource
+	 *
+	 * @param pattern
+	 */
+	var getAssets = function (pattern) {
+		var files = [];
+		if (_.isArray(pattern)) {
+			_.each(pattern, function (path) {
+				files = files.concat(getAssets(path));
+			});
+		} else if (_.isString(pattern)) {
+			var regex = new RegExp('^(http://|https://|//)');
+			if (regex.test(pattern)) {
+				// Source is external
+				files.push(pattern);
+			} else {
+				glob(pattern, globOptions, function (er, matches) {
+					files = filterFiles(matches);
+				});
+			}
+		}
 
-    return files;
-  }
+		return files;
+	};
 
-  // Get CSS
-  _.each(options.css, function (value, key) {
-    if (!options.debug) {
-      assets.css.push(key);
-    } else {
-      assets.css = assets.css.concat(getAssets(value));
-    }
-  });
+	_.each(['css', 'js'], function (fileType) {
+		_.each(options[fileType], function (value, key) {
+			if (!options.debug) {
+				assets[fileType].push(key);
+			} else {
+				assets[fileType] = assets[fileType].concat(getAssets(value));
+			}
+		});
+		if (options.webroot) {
+			// Strip the webroot foldername from the filepath
+			var regex = new RegExp('^' + options.webroot);
+			_.each(assets[fileType], function (value, key) {
+				assets[fileType][key] = value.replace(regex, '');
+			});
+		}
+	});
 
-  // Get JavaScript
-  _.each(options.js, function (value, key) {
-    if (!options.debug) {
-      assets.js.push(key);
-    } else {
-      assets.js = assets.js.concat(getAssets(value));
-    }
-  });
-
-  if (options.webroot) {
-    // Strip the webroot foldername from the filepath
-    var regex = new RegExp('^' + options.webroot);
-    _.each(assets.css, function (value, key) {
-      assets.css[key] = value.replace(regex, '');
-    });
-    _.each(assets.js, function (value, key) {
-      assets.js[key] = value.replace(regex, '');
-    });
-  }
 };
 
 exports.assets = assets;
